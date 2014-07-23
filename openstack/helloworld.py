4a32191db187e55050a12a9850e12824ff8c5942
Mike Hagedorn <mike@silverchairsolutions.com>
2014-07-23 12:29:38 -0400

    added openstack, .gitignore

---
+++ openstack/helloworld.py
@@ -0,0 +1,16 @@
+def print_values(a, b, c):
+    print "You're %r years old, %r m tall and %r kg." % (a, b, c)
+
+
+def main():
+    print "How old are you, Mike?",
+    age = raw_input()
+    print "How Tall are you?",
+    height = raw_input()
+    print "How much do you weigh?",
+    weight = raw_input()
+    print_values(age, height, weight)
+
+
+if __name__ == '__main__':
+    main()
